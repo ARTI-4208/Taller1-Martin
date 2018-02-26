058fa1295049147e531b18c5547d5c56b474205c
morjuela <36166277+morjuela@users.noreply.github.com>
2018-02-26 00:45:29 -0500

    Microservicios

--- deployments-apigateway/registry.js
+++ deployments-apigateway/registry.js
@@ -1,12 +1,12 @@
 var requestify     = require("requestify");
 
-const apiGatewayUrl = "http://172.31.9.84:31602/apps"; //direcciOn IP y el puerto donde se estA ejecutando el servicio del apigateway
+const apiGatewayUrl = "http://172.24.101.92:30506/apps"; //direcciOn IP y el puerto donde se estA ejecutando el servicio del apigateway
 console.log("Registering microservice of insumos " + apiGatewayUrl);
 requestify.request(apiGatewayUrl, {
     method: "POST",
     body: {
         "appName": "microsvinsumos",
-        "hostName": "172.31.9.84", //direcciOn ip del worker deon se estA ejecutando el microservicio
+        "hostName": "172.31.9.84", //direcciOn ip del worker donde se estA ejecutando el microservicio
         "port": 30385, //puerto por el que el servicio balanceador de carga de la aplicaciOn microservicio1 está escuchando
         "service": "/microsvinsumos",
         "method": "GET"
